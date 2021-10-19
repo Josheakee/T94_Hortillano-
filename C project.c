2520b8b4c958c4b0e0cce1125da63617594c4b52
Joshea Kee Hortillano <89246901+Josheakee@users.noreply.github.com>
2021-10-19 13:51:28 +0800

    Add files via upload

---
+++ C project.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    char characterName[] = "Joshea Kee Hortillano ";
+     int characterAge = 18;
+    printf(" %s\n", characterName  );
+    printf(" %d years old.\n", characterAge);
+
+
+
+    return 0;
+}
